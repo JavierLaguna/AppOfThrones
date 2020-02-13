6302bd358119dda6a9ff670ac73c1604042ecb0f
Javier Laguna <javier.laguna92@gmail.com>
2020-02-13 22:27:41 +0100

    Add EpisodeViewController and Episode model

---
+++ AppOfThrones/Episodes/EpisodeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EpisodeViewController.swift
+//  AppOfThrones
+//
+//  Created by Javier Laguna on 13/02/2020.
+//  Copyright © 2020 Javier Laguna. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeViewController: UIViewController {
+    
+    // MARK: Variables
+    var episodes: [Episode] = [Episode(id: 1, name: "Winter is Coming", date: "April 17, 2011", image: "episodeTest", episode: 1, season: 1, overview: "Jon Arryn, the Hand of the King, is dead. King Robert...")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ AppOfThrones/Model/Episode.swift
@@ -0,0 +1,38 @@
+//
+//  Episode.swift
+//  AppOfThrones
+//
+//  Created by Javier Laguna on 13/02/2020.
+//  Copyright © 2020 Javier Laguna. All rights reserved.
+//
+
+import Foundation
+
+class Episode {
+    
+    let id: Int
+    let name: String?
+    let date: String?
+    let image: String?
+    let episode: Int
+    let season: Int
+    let overview: String
+    
+    init(id: Int,
+         name: String?,
+         date: String?,
+         image: String?,
+         episode: Int,
+         season: Int,
+         overview: String) {
+        
+        self.id = id
+        self.name = name
+        self.date = date
+        self.image = image
+        self.episode = episode
+        self.season = season
+        self.overview = overview
+    }
+    
+}
