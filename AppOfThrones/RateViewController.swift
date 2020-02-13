b0dfcfccfd426bc59a2259f7753f3ed070721b45
Javier Laguna <javier.laguna92@gmail.com>
2020-02-13 22:00:07 +0100

    Add RateViewController

---
+++ AppOfThrones/RateViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RateViewController.swift
+//  AppOfThrones
+//
+//  Created by Javier Laguna on 11/02/2020.
+//  Copyright © 2020 Javier Laguna. All rights reserved.
+//
+
+import UIKit
+
+class RateViewController: UIViewController {
+    
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var confirmButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        confirmButton.layer.cornerRadius = 4.0
+    }
+    
+    @IBAction func tapButton(_ sender: Any) {
+        print("tap")
+    }
+}
