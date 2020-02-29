f97d28fb8a9d5fbc21f64cf19ef789fc14ed7290
Javier Laguna <javier.laguna92@gmail.com>
2020-02-29 10:16:13 +0100

    Add Cast and Houses to favorites screen

--- AppOfThrones/Scenes/Favorites/FavoritesViewController.swift
+++ AppOfThrones/Scenes/Favorites/FavoritesViewController.swift
@@ -18,13 +18,35 @@ final class FavoritesViewController: UIViewController, TabBarItemable {
     
     var tbTitle: String { "Favorites" }
     var tbImage: UIImage? { UIImage(systemName: "heart.fill") }
+    
     private var allEpisodes = [Episode]() {
         didSet {
             favoritesTable.reloadData()
         }
     }
+    
     private var favoriteEpisodes: [Episode] {
         return allEpisodes.filter { DataController.shared.isFavorite($0) }
+    }
+    
+    private var cast = [Cast]() {
+        didSet {
+            favoritesTable.reloadData()
+        }
+    }
+    
+    private var favoriteCast: [Cast] {
+        return cast.filter { DataController.shared.isFavorite($0) }
+    }
+    
+    private var houses = [House]() {
+        didSet {
+            favoritesTable.reloadData()
+        }
+    }
+    
+    private var favoriteHouses: [House] {
+        return houses.filter { DataController.shared.isFavorite($0) }
     }
     
     // MARK: LifeCycle
@@ -48,6 +70,8 @@ final class FavoritesViewController: UIViewController, TabBarItemable {
         favoritesTable.delegate = self
         
         favoritesTable.register(UINib(nibName: EpisodeTableViewCell.nibName, bundle: nil), forCellReuseIdentifier: EpisodeTableViewCell.defaultReuseIdentifier)
+        favoritesTable.register(UINib(nibName: CastTableViewCell.nibName, bundle: nil), forCellReuseIdentifier: CastTableViewCell.defaultReuseIdentifier)
+        favoritesTable.register(UINib(nibName: HouseTableViewCell.nibName, bundle: nil), forCellReuseIdentifier: HouseTableViewCell.defaultReuseIdentifier)
         
         favoritesTable.tableFooterView = UIView()
     }
@@ -62,6 +86,8 @@ final class FavoritesViewController: UIViewController, TabBarItemable {
     
     private func getData() {
         allEpisodes = DataController.shared.getAllEpisodes()
+        cast = DataController.shared.getCast()
+        houses = DataController.shared.getHouses()
     }
     
 }
@@ -70,11 +96,50 @@ final class FavoritesViewController: UIViewController, TabBarItemable {
 
 extension FavoritesViewController: UITableViewDataSource {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return favoriteEpisodes.count
+        switch section {
+        case 0:
+            return favoriteEpisodes.count
+        case 1:
+            return favoriteCast.count
+        case 2:
+            return favoriteHouses.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Episodes"
+        case 1:
+            return "Cast"
+        case 2:
+            return "Houses"
+        default:
+            return ""
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            return getEpisodeCell(tableView, cellForRowAt: indexPath)
+        case 1:
+            return getCastCell(tableView, cellForRowAt: indexPath)
+        case 2:
+            return getHouseCell(tableView, cellForRowAt: indexPath)
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+    private func getEpisodeCell(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell  {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: EpisodeTableViewCell.defaultReuseIdentifier, for: indexPath) as? EpisodeTableViewCell else {
             return UITableViewCell()
         }
@@ -84,6 +149,27 @@ extension FavoritesViewController: UITableViewDataSource {
         cell.delegate = self
         return cell
     }
+    
+    private func getCastCell(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell  {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CastTableViewCell.defaultReuseIdentifier, for: indexPath) as? CastTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let cast = favoriteCast[indexPath.row]
+        cell.setCast(cast)
+        cell.delegate = self
+        return cell
+    }
+    
+    private func getHouseCell(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell  {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HouseTableViewCell.defaultReuseIdentifier, for: indexPath) as? HouseTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let house = favoriteHouses[indexPath.row]
+        cell.setHouse(house)
+        return cell
+    }
 }
 
 // MARK: UITableViewDelegate
@@ -91,14 +177,59 @@ extension FavoritesViewController: UITableViewDataSource {
 extension FavoritesViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 128
+        switch indexPath.section {
+        case 0:
+            return 128
+        case 1:
+            return 148
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
+        headerView.backgroundColor = UIColor.darkGray
+        
+        let label = UILabel()
+        label.frame = CGRect.init(x: 5, y: -6, width: headerView.frame.width-10, height: headerView.frame.height-10)
+        label.text = labelForHeader(section)
+        label.font = .boldSystemFont(ofSize: 16)
+        label.textColor = UIColor.orangeMain
+        
+        headerView.addSubview(label)
+        
+        return headerView
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let episode = favoriteEpisodes[indexPath.row]
-        let detailVC = EpisodeDetailViewController(withEpisode: episode)
-        
-        self.navigationController?.present(UINavigationController(rootViewController: detailVC), animated: true, completion: nil)
+        switch indexPath.section {
+        case 0:
+            let episode = favoriteEpisodes[indexPath.row]
+            let detailVC = EpisodeDetailViewController(withEpisode: episode)
+            
+            self.navigationController?.present(UINavigationController(rootViewController: detailVC), animated: true, completion: nil)
+        case 1:
+            let cast = favoriteCast[indexPath.row]
+            let detailVC = CastDetailViewController(withCast: cast)
+            
+            self.navigationController?.present(UINavigationController(rootViewController: detailVC), animated: true, completion: nil)
+        default:
+            return
+        }
+    }
+    
+    private func labelForHeader(_ section: Int) -> String {
+        switch section {
+        case 0:
+            return "Episodes"
+        case 1:
+            return "Cast"
+        case 2:
+            return "Houses"
+        default:
+            return ""
+        }
     }
 }
 
